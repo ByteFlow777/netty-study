4efc5b1876bedd445fb7d60e0fe7b2960e8ee17e
Leon Li <byteflow777@gmail.com>
2022-01-30 16:23:34 +0800

    BetterFileChannelTransferTo 利用 Channel 传输超过 2G 的数据

--- src/test/java/io/byteflow777/c1/TestFileChannelTransferTo.java
+++ src/test/java/io/byteflow777/c1/TestFileChannelTransferTo.java
@@ -19,4 +19,21 @@ public class TestFileChannelTransferTo {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testBetterFileChannelTransferTo() {
+        // 由于 transferTo 一次最多传输大约 2G 数据
+        // 如果有大于 2G 的数据可以多次传输
+        try (
+                FileChannel from = new FileInputStream("data.txt").getChannel();
+                FileChannel to = new FileOutputStream("to.txt").getChannel()
+        ) {
+            long size = from.size(), left = size;
+            while (left > 0) {
+                left -= from.transferTo(size - left, left, to);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
